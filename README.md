4959eadd93a38c205210dcc362dc3956d3b19b27
5alt <md5_salt@qq.com>
2017-09-28 16:51:54 +0800

    first

---
+++ README.md
@@ -0,0 +1,17 @@
+# chrome-sec-disable
+
+Disable some security features in Chromium browers for web application testing.
+
+* Content-Security-Policy
+* X-Frame-Options
+* X-XSS-Protection
+* X-Content-Type-Options
+* X-Permitted-Cross-Domain-Policies
+
+Based on https://github.com/PhilGrayson/chrome-csp-disable
+
+## Contributors
+
+* [Phil Grayson](https://github.com/PhilGrayson)
+* [Denis Gorbachev](https://github.com/DenisGorbachev)
+* [md5_salt](https://github.com/5alt)
---
+++ background.js
@@ -0,0 +1,54 @@
+//https://www.owasp.org/index.php/OWASP_Secure_Headers_Project
+
+var isSecDisabled = false;
+
+var onHeadersReceived = function(details) {
+  if (!isSecDisabled) {
+      return;
+  }
+
+  for (var i = 0; i < details.responseHeaders.length; i++) {
+    if ('content-security-policy' === details.responseHeaders[i].name.toLowerCase()) {
+      details.responseHeaders[i].value = '';
+    }else if ('x-frame-options' === details.responseHeaders[i].name.toLowerCase()) {
+      details.responseHeaders[i].value = 'allow-from: *';
+    }else if ('x-xss-protection' === details.responseHeaders[i].name.toLowerCase()) {
+      details.responseHeaders[i].value = '0';
+    }else if ('x-content-type-options' === details.responseHeaders[i].name.toLowerCase()) {
+      details.responseHeaders[i].value = 'sniff';
+    }else if ('x-permitted-cross-domain-policies' === details.responseHeaders[i].name.toLowerCase()) {
+      details.responseHeaders[i].value = 'all';
+    }
+  }
+
+  return {
+    responseHeaders: details.responseHeaders
+  };
+};
+
+var updateUI = function() {
+  var iconName = isSecDisabled ? 'on' : 'off';
+  var title    = isSecDisabled ? 'disabled' : 'enabled';
+
+  chrome.browserAction.setIcon({ path: "images/icon38-" + iconName + ".png" });
+  chrome.browserAction.setTitle({ title: 'Content-Security-Policy headers are ' + title });
+};
+
+var filter = {
+  urls: ["*://*/*"],
+  types: ["main_frame", "sub_frame"]
+};
+
+chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, filter, ["blocking", "responseHeaders"]);
+
+chrome.browserAction.onClicked.addListener(function() {
+  isSecDisabled = !isSecDisabled;
+
+  if (isSecDisabled) {
+    chrome.browsingData.remove({}, {"serviceWorkers": true}, function () {});
+  }
+
+  updateUI()
+});
+
+updateUI();
